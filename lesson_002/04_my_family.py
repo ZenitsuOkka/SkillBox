4d77f5a9030c75538c12a7ba3ce3a4193727a114
Andrey <zenitsuokka@gmail.com>
2021-10-13 10:55:47 +0300

    Рост всей семьи

--- lesson_002/04_my_family.py
+++ lesson_002/04_my_family.py
@@ -20,3 +20,11 @@ my_family_height = [
 print('Рост: '+my_family[0]+' -', my_family_height[0][1],'см.')
 # Выведите на консоль общий рост вашей семьи как сумму ростов всех членов
 #   Общий рост моей семьи - ХХ см
+
+sum_height = 0
+sum_height += my_family_height[0][1]
+sum_height += my_family_height[1][1]
+sum_height += my_family_height[2][1]
+
+
+print("Общий рост семьи -", sum_height,"см")
