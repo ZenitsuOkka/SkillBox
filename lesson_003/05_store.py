7ac305458d91de842fbf096d4d03f16cb99d5b6b
Andrey <zenitsuokka@gmail.com>
2021-10-18 13:35:30 +0300

    complit

--- lesson_003/05_store.py
+++ lesson_003/05_store.py
@@ -13,20 +13,20 @@ goods = {
 
 store = {
     '12345': [
-        {'quantity': 27, 'price': 42},
+        {'quality': 27, 'price': 42},
     ],
     '23456': [
-        {'quantity': 22, 'price': 510},
-        {'quantity': 32, 'price': 520},
+        {'quality': 22, 'price': 510},
+        {'quality': 32, 'price': 520},
     ],
     '34567': [
-        {'quantity': 2, 'price': 1200},
-        {'quantity': 1, 'price': 1150},
+        {'quality': 2, 'price': 1200},
+        {'quality': 1, 'price': 1150},
     ],
     '45678': [
-        {'quantity': 50, 'price': 100},
-        {'quantity': 12, 'price': 95},
-        {'quantity': 43, 'price': 97},
+        {'quality': 50, 'price': 100},
+        {'quality': 12, 'price': 95},
+        {'quality': 43, 'price': 97},
     ],
 }
 
@@ -47,6 +47,18 @@ store = {
 #     вывод на консоль количества и стоимости товара на складе
 
 # TODO здесь ваш код
+item_of_goods = []
+
+for product_name, product_code in goods.items():
+
+    product_quality = 0
+    product_value = 0
+
+    for position in store[product_code]:
+        product_quality += position['quality']
+        product_value += position['quality'] * position['price']
+
+    print(product_name, '-', product_quality, 'шт, стоимость', product_value, 'руб')
 
 
 
