683cc71125f31e6501889e2044dd73f47511aad3
Andrey <zenitsuokka@gmail.com>
2021-10-27 14:15:07 +0300

    ready

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -4,6 +4,8 @@
 # Формат: В комнате room_1 живут: ...
 
 import room_1 as f1
+inhabitant1 = ', '.join(f1.folks)
 import room_2 as f2
-print('В комнате 1 живут', f1)
-print('В комнате 2 живут', f2)
+inhabitant2 = ', '.join(f2.folks)
+print('В комнате 1 живут:', inhabitant1)
+print('В комнате 2 живут:', inhabitant2)
