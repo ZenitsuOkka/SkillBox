406ba1df4a733459bc09d709fbb4614dcadd8d47
Andrey <zenitsuokka@gmail.com>
2021-11-24 16:38:17 +0200

    parse events.txt

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -18,8 +18,47 @@
 #
 # Входные параметры: файл для анализа, файл результата
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
+import os
 
-# TODO здесь ваш код
+
+class LogParser:
+
+    def __init__(self, file_in=None, file_out=None):
+        self.file_in = file_in
+        self.file_out = file_out
+        self.log_stat = {}
+
+        if not self.file_out:
+            self.file_out = self.file_in + '.nok'
+
+        if os.path.exists(self.file_out):
+            os.remove(self.file_out)
+
+    def line_parsing(self, line=None):
+        date, time, res = line.split(' ')
+        dt = date + ' ' + time[:5] + time[-1:]
+        return dt, res
+
+    def parse(self):
+        with open(self.file_in, 'r') as file:
+            for line in file:
+                dt, res = self.line_parsing(line)
+                res = res.replace('\n', '')
+                if res =='NOK':
+                    if dt not in self.log_stat.keys():
+                        self.write()
+                        self.log_stat = {}
+                    self.log_stat[dt] = self.log_stat.setdefault(dt, 0) + 1
+
+    def write(self):
+        with open(file=self.file_out, mode='a', encoding='utf8') as file:
+            for date, cnt in self.log_stat.items():
+                file.write(f'{date} {cnt} \n')
+
+if __name__ == '__main__':
+    log = LogParser('events.txt')
+    log.parse()
+
 
 # После выполнения первого этапа нужно сделать группировку событий
 #  - по часам
